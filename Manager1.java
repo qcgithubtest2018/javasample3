ae10ec820dc267b0110260fbe2ae8512da06bf02
qcgithubtest2018 <106365872+qcgithubtest2018@users.noreply.github.com>
2022-05-28 12:30:33 +0530

    Add files via upload

---
+++ Manager1.java
@@ -0,0 +1,13 @@
+package pkg1;
+
+public class Manager1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("OK");
+		System.out.println("Done");
+	}
+}
